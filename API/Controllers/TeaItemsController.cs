15b9db562ddda27504dd54ca97478b10bc1d7978
sofia-iliana <sofia.iliana1311@gmail.com>
2024-07-05 12:04:48 +0300

    Backend initial

---
+++ API/Controllers/TeaItemsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TeaItemsController(DataContext context) : ControllerBase
+{
+    //private readonly DataContext _context = context;
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<TeaItem>>> GetItems()
+    {
+        var teaItems = await context.TeaItems.ToListAsync();
+
+        return teaItems;
+    }
+
+}
---
+++ API/Entities/TeaItem.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities;
+
+public class TeaItem
+{
+    public int Id {get; set;}
+    public required string Name {get; set;}
+    public required string Type {get; set;}
+    public required string Description {get; set;}
+    public decimal Price {get; set;}
+    public required string Image {get; set;}
+}
